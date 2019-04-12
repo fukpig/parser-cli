d52e408a81a07dd33be7838656b4f692519bc12b
Rinat Tleukulov <fukpig2@gmail.com>
2019-04-12 17:57:04 +0600

    add timeout and limits

--- parser.go
+++ parser.go
@@ -7,19 +7,27 @@ import (
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 )
 
 func parse(urls string, searchUrl string) {
+
 	result := make(map[string]int)
 	var urlList []string
 	urlList = strings.Split(urls, ",")
 
 	findSubstringRegExp := regexp.MustCompile(searchUrl)
 
+	limiter := time.Tick(time.Second * 1)
+
 	var wg sync.WaitGroup
 	for _, url := range urlList {
 		wg.Add(1)
+
+		timeout := make(chan string, 1)
 		go func(urlA string, substringRegExp *regexp.Regexp) {
+			<-limiter
+			time.Sleep(4 * time.Second)
 			defer wg.Done()
 
 			resp, err := http.Get(urlA)
@@ -41,12 +49,19 @@ func parse(urls string, searchUrl string) {
 					matches := findSubstringRegExp.FindAllStringIndex(html, -1)
 					matchesCount = len(matches)
 				}
-
+				timeout <- "hello"
 				result[urlA] = matchesCount
 			}
 		}(url, findSubstringRegExp)
-	}
 
+		select {
+		case res := <-timeout:
+			fmt.Println(res)
+		case <-time.After(500 * time.Millisecond):
+			fmt.Println("timeout")
+		}
+
+	}
 	wg.Wait()
 
 	for key, value := range result {
