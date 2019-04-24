aeca7bcfdcec7b4570193583012eab9cb4b2ab0e
Rinat Tleukulov <fukpig2@gmail.com>
2019-04-25 03:35:11 +0600

    another try

--- parser.go
+++ parser.go
@@ -3,9 +3,11 @@ package parsercli
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -22,7 +24,6 @@ type parserStruct struct {
 }
 
 func scrap(ctx context.Context, client *http.Client, timeout int, targetURL string) string {
-
 	ctxTimeout, timeoutCancel := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
 	defer timeoutCancel()
 
@@ -33,7 +34,6 @@ func scrap(ctx context.Context, client *http.Client, timeout int, targetURL stri
 	}
 
 	resp, err := client.Do(req.WithContext(ctxTimeout))
-
 	if err != nil {
 		fmt.Println(err)
 		return ""
@@ -49,161 +49,109 @@ func scrap(ctx context.Context, client *http.Client, timeout int, targetURL stri
 	return html
 }
 
-func getUrls(ctx context.Context, urls string) chan string {
-	var wg sync.WaitGroup
+func countMatches(html string, substringRegExp *regexp.Regexp) int {
+	matches := substringRegExp.FindAllStringIndex(html, -1)
+	matchesCount := len(matches)
+	return matchesCount
+}
+
+func getUrls(ctx context.Context, wg *sync.WaitGroup, urls string) chan string {
 
 	urlList := strings.Split(urls, ",")
 	urlsChan := make(chan string, len(urlList))
 
-	defer close(urlsChan)
-	for _, url := range urlList {
-		wg.Add(1)
-		go func(ctx context.Context, urlsChan chan string, url string) {
-			defer wg.Done()
+	wg.Add(1)
+	go func(ctx context.Context, urlsChan chan string, urlList []string) {
+		defer wg.Done()
+		defer close(urlsChan)
+
+		for _, url := range urlList {
+			fmt.Println("GET", url)
 			urlsChan <- url
-		}(ctx, urlsChan, url)
-	}
-	wg.Wait()
+		}
+	}(ctx, urlsChan, urlList)
+
 	return urlsChan
 }
 
-func getHTML(ctx context.Context, urlsChan chan string, timeout int) chan parserStruct {
-	var wg sync.WaitGroup
+func getHTML(ctx context.Context, wg *sync.WaitGroup, urlsChan chan string, timeout int) chan parserStruct {
 	htmlChan := make(chan parserStruct)
 	client := &http.Client{}
 
-	wg.Add(1)
-	go func(ctx context.Context, client *http.Client, timeout int) {
+	go func(ctx context.Context, urlsChan chan string) {
 		for url := range urlsChan {
-			fmt.Println("get html", url)
-			html := scrap(ctx, client, timeout, url)
-			htmlChan <- parserStruct{url: url, html: html}
+			wg.Add(1)
+			go func(ctx context.Context, wg *sync.WaitGroup, client *http.Client, timeout int, url string) {
+				defer wg.Done()
+				html := scrap(ctx, client, timeout, url)
+				htmlChan <- parserStruct{url: url, html: html}
+				fmt.Println("SCRAP", url)
+
+			}(ctx, wg, client, timeout, url)
 		}
-		defer wg.Done()
-	}(ctx, client, timeout)
-	go func() {
-		wg.Wait()
-		close(htmlChan)
-		fmt.Println("finished")
-	}()
+		defer close(htmlChan)
+	}(ctx, urlsChan)
+	//defer close(htmlChan)
 
 	return htmlChan
 }
 
-func parseHTML(ctx context.Context, htmlChan chan parserStruct, searchString string) chan string {
-	fmt.Println("parse")
-	var wg sync.WaitGroup
+func parseHTML(ctx context.Context, wg *sync.WaitGroup, htmlChan chan parserStruct, searchString string) chan resultStruct {
+	occurrencesChan := make(chan resultStruct)
+	findSubstringRegExp := regexp.MustCompile(searchString)
 
-	occurrencesChan := make(chan string)
-
-	wg.Add(1)
-	go func() {
-		for parserInfo := range htmlChan {
-			//go func(htmlChan chan string, url string) {
-			fmt.Println("parse html", parserInfo)
-			occurrencesChan <- parserInfo.url
-			//}(occurrencesChan, url)
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		wg.Wait()
-		close(occurrencesChan)
-	}()
+	for parserInfo := range htmlChan {
+		//wg.Add(1)
+		fmt.Println("PARSE", parserInfo.url)
+		count := countMatches(parserInfo.html, findSubstringRegExp)
+		occurrencesChan <- resultStruct{url: parserInfo.url, count: count}
+		/*go func(ctx context.Context, wg *sync.WaitGroup, parserInfo parserStruct, findSubstringRegExp *regexp.Regexp) {
+			defer wg.Done()
+			count := countMatches(parserInfo.html, findSubstringRegExp)
+			occurrencesChan <- resultStruct{url: parserInfo.url, count: count}
+		}(ctx, wg, parserInfo, findSubstringRegExp)*/
+	}
+	close(occurrencesChan)
 
 	return occurrencesChan
 }
 
 //render generate output Url - count
-func render(ctx context.Context, occurrencesChan chan string) {
+func render(ctx context.Context, wg *sync.WaitGroup, occurrencesChan chan resultStruct) {
 	fmt.Println("render")
-	var mx sync.RWMutex
-	go func(occurrencesChan chan string) {
-		for url := range occurrencesChan {
-			mx.Lock()
-			fmt.Println("render html", url)
-			mx.Unlock()
-		}
-	}(occurrencesChan)
+	for resultStruct := range occurrencesChan {
+		fmt.Println("123")
+		fmt.Println(resultStruct.url, "-", resultStruct.count)
+	}
 }
 
-/*func render(urlsResult map[string]int) {
-	for key, value := range urlsResult {
-		fmt.Println(key, " - ", value)
-	}
-}*/
+func shutdown(ctx context.Context, wg *sync.WaitGroup, htmlChan chan parserStruct, occurrencesChan chan resultStruct) error {
+	ch := make(chan struct{})
 
-//scrapCount get html by url and find by regexp count matches
-/*func scrapCount(
-	ctx context.Context,
-	client *http.Client,
-	targetURL string,
-	substringRegExp *regexp.Regexp,
-	timeout int,
-) int {
-	ctxTimeout, timeoutCancel := context.WithTimeout(ctx, time.Duration(timeout)*time.Second)
-	defer timeoutCancel()
+	go func() {
+		wg.Wait()
+		close(htmlChan)
+		close(occurrencesChan)
+		close(ch)
+	}()
 
-	req, err := http.NewRequest("GET", targetURL, nil)
-	if err != nil {
-		fmt.Println(err)
-		return 0
+	select {
+	case <-ch:
+		return nil
+	case <-ctx.Done():
+		return errors.New("timeout")
 	}
-
-	resp, err := client.Do(req.WithContext(ctxTimeout))
-	if err != nil {
-		fmt.Println(err)
-		return 0
-	}
-	defer resp.Body.Close()
-	htmlBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return 0
-	}
-
-	html := string(htmlBytes)
-	matchesCount := 0
-	if html != "" {
-		matches := substringRegExp.FindAllStringIndex(html, -1)
-		matchesCount = len(matches)
-	}
-	return matchesCount
-}*/
+}
 
 //Parse get array of urls and parse them to find occurrences of search string
 func Parse(ctx context.Context, urls, searchString string, maxGoroutines, timeout int) {
-	urlsChan := getUrls(ctx, urls)
-	htmlChan := getHTML(ctx, urlsChan, timeout)
-	occurrencesChan := parseHTML(ctx, htmlChan, searchString)
-	render(ctx, occurrencesChan)
-	/*limiter := make(chan struct{}, maxGoroutines)
-
-	urlList := strings.Split(urls, ",")
-
-	findSubstringRegExp := regexp.MustCompile(searchURL)
-
 	var wg sync.WaitGroup
 
-	client := &http.Client{}
+	urlsChan := getUrls(ctx, &wg, urls)
+	htmlChan := getHTML(ctx, &wg, urlsChan, timeout)
+	occurrencesChan := parseHTML(ctx, &wg, htmlChan, searchString)
+	render(ctx, &wg, occurrencesChan)
 
-	result := resultStruct{urlsWithCount: make(map[string]int, len(urlList))}
-	for _, url := range urlList {
-		wg.Add(1)
-		go func(ctx context.Context, targetURL string, substringRegExp *regexp.Regexp, timeout int) {
-			defer wg.Done()
-			defer func(ch <-chan struct{}) {
-				<-ch
-			}(limiter)
-			limiter <- struct{}{}
+	shutdown(ctx, &wg, htmlChan, occurrencesChan)
 
-			count := scrapCount(ctx, client, targetURL, substringRegExp, timeout)
-
-			result.addCountToURL(targetURL, count)
-		}(ctx, url, findSubstringRegExp, timeout)
-	}
-	wg.Wait()
-
-	render(result.urlsWithCount)*/
 }
